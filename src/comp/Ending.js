7670b7ab786c18896d14f44c755431238c51d319
Adithyan Ajish <85665413+manuqwert1234@users.noreply.github.com>
2024-06-23 15:29:41 +0400

    Update Ending.js

--- src/comp/Ending.js
+++ src/comp/Ending.js
@@ -1,26 +1,23 @@
 import React from 'react'
 import '../style/Ending.css'
+import video from '../comp/blob-1-opacity-33.gif'
 
 function Ending() {
   return (
-    <div>
-      <div className='ending'>
-        <h1>Mission & Objective</h1>
-        <div className='ending-middle'>
-          <h2>Our mission is to empower students with practical data science skills, promote interdisciplinary learning.</h2>
-          <h3>We strive to organize impactful events, workshops, and hackathons that enhance our members' data science knowledge, foster a collaborative environment, and prepare them for real-world data challenges.</h3>
-        </div>
-
-
-      </div>
-     
-
-      
-    
+    <section className='blackcolorballsnotused'>
+      <div id="Ending">
         
-  
-    </div>
+        <img src={video} className='video' />
+        <div className='ending'>
+          <div className='ending-content'>
+            <h1>Mission & Objective</h1>
+            <h2>Our mission is to empower students with practical data science skills, promote interdisciplinary learning.</h2>
+            <h3>We strive to organize impactful events, workshops, and hackathons that enhance our members' data science knowledge, foster a collaborative environment, and prepare them for real-world data challenges.</h3>
+          </div>
+        </div>
+      </div>
+    </section>
   )
-}
+} 
 
 export default Ending
